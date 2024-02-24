287ada01727a173e109174eef699d404a35eac26
he/him <132726465+baoduyne@users.noreply.github.com>
2024-02-25 02:51:53 +0700

    Add files via upload

---
+++ firstjava.java
@@ -0,0 +1,8 @@
+public class firstjava {
+    public int i = 0 ;
+    public static void main(String[] args){
+        firstjava f = new firstjava();
+            f.i ++;
+            System.out.println(f.i);
+    }
+}
---
+++ second.java
@@ -0,0 +1,9 @@
+public class second {
+    public static void main(String[] args) {
+        student std = new student(5,"Duy");
+        System.out.println(std.id +" " + std.a +" " +std.b);
+        school scc = new school("PKA");
+        scc.show();
+    }
+    
+}
