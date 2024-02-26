a1ee9803cae93baecfee72906924ed9d6b69656a
he/him <132726465+baoduyne@users.noreply.github.com>
2024-02-26 23:10:05 +0700

    Add files via upload

---
+++ in.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+    public class in {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+            System.out.println("Name: ");
+                String name = " ";
+                name = scan.nextLine();
+            System.out.println("Your name is : " + name);
+    }
+    }
